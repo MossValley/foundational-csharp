66e704c498967eb2bd1a3bad529a1bc09dfe133f
Moss <ramivalmont@gmail.com>
2024-09-12 16:25:58 +1200

    Challenge: Improve renewal rate of subscriptions
    code challenge

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,43 +1,29 @@
-﻿Random dice = new Random();
+﻿Random random =  new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+daysUntilExpiration = 1;
 
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+if ((daysUntilExpiration <= 10) && (daysUntilExpiration > 5))
 {
-
-  if ((roll1 == roll2) && (roll2 == roll3))
-  {
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-  }
-  else 
-  {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-  }
-
-  Console.WriteLine($"Your total including the bonus: {total}");
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if ((daysUntilExpiration <= 5) && (daysUntilExpiration > 1))
+{
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+  discountPercentage = 10;
+}
+else if (daysUntilExpiration == 1)
+{
+  Console.WriteLine("Your subscription expires within a day!");
+  discountPercentage = 20;
+}
+else if (daysUntilExpiration == 0)
+{
+  Console.WriteLine("Your subscription has expired.");
 }
 
-if (total >= 16)
+if (discountPercentage > 0)
 {
-  Console.WriteLine("You win a new car!");
-}
-else if (total >= 10) 
-{
-  Console.WriteLine("You win a new laptop!");
-}
-else if (total >= 7)
-{
-  Console.WriteLine("You win a trip for two!");
-}
-else
-{
-  Console.WriteLine("You win a kitten!");
+  Console.WriteLine($"Renew now and save {discountPercentage}%!");
 }
