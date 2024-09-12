50d5cb01527f33be12cf10aad1ef7a1e9a57a2a8
Moss <ramivalmont@gmail.com>
2024-09-12 15:49:59 +1200

    create decision logic with if statements

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,7 +1,31 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+﻿Random dice = new Random();
 
-largerValue = Math.Max(firstValue, secondValue);
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine(largerValue);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
+{
+  Console.WriteLine("You rolled doubles! +2 bonus to total!");
+  total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+  Console.WriteLine("You rolled triples! +6 bonus to total!");
+  total += 6;
+}
+
+if (total >= 15)
+{
+  Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+  Console.WriteLine("Sorry, you lose.");
+}
