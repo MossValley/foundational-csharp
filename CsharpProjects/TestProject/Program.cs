897d12bda9b721d9bad05fa4b4e0750b65e26e73
Moss <ramivalmont@gmail.com>
2024-09-19 14:31:48 +1200

    challenge: conditional operator

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,23 +1,3 @@
-﻿/*
-  This code reverses a string and counts the number
-  of times a particular character appears.
-*/
-using Microsoft.VisualBasic;
-
-string input = "The quick brown fox jumps over the lazy dog.";
-
-char[] message = input.ToCharArray();
-Array.Reverse(message);
-
-int count = 0;
-foreach (char letter in message)
-{ 
-  if (letter == 'o')
-  { 
-    count++; 
-  } 
-}
-
-string newMessage = new String(message);
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {count} times.");
+﻿Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine(flip > 0 ? "heads" : "tails");
