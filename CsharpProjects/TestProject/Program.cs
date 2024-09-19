c33b07869ade797844131bf9dd33ee405c19b84b
Moss <ramivalmont@gmail.com>
2024-09-19 14:46:45 +1200

    challenge: decision logic

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,16 @@
-﻿Random coin = new Random();
-int flip = coin.Next(0, 2);
-Console.WriteLine(flip > 0 ? "heads" : "tails");
+﻿string permission = "Admin";
+int level = 65;
+
+string message = "";
+
+if (permission.Contains("Admin")) {
+  message = level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.";
+}
+else if (permission.Contains("Manager")) {
+  message = level > 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.";
+}
+else {
+  message = "You do not have sufficient privileges.";
+}
+
+Console.WriteLine(message);
