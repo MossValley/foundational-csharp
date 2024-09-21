0c7781f70152061db4fe609a3e4f151809c209a9
Moss <ramivalmont@gmail.com>
2024-09-21 16:40:09 +1200

    fixup! challege: Code project 1 - write code that validates integer input

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -2,10 +2,9 @@
 Console.WriteLine("Enter an integer value between 5 and 10");
 
 int numberValue = 0;
-bool isNumber = false;
-bool isValidNumber = false;
+bool isNumber;
 
-do
+while (numberValue < 5 || numberValue > 10)
 {
     userInput = Console.ReadLine();
     isNumber = int.TryParse(userInput, out numberValue);
@@ -14,15 +13,11 @@ do
     {
         if (numberValue < 5 || numberValue > 10)
             Console.WriteLine($"You entered {userInput}. Please enter a number between 5 and 10");
-        else
-            isValidNumber = true;
     }
     else
     {
         Console.WriteLine("Sorry, you entered an invalid number, please try again");
     }
-
-
-} while (!isValidNumber);
+}
 
 Console.WriteLine($"Your input value ({numberValue}) has been accepted");
